6103405c2936b001ae17577fc949eb0b8d6f01f6
wanghan <>
2024-02-27 20:40:06 +0800

    制作移动平台的吸附功能

--- Assets/Scripts/PlatformController.cs
+++ Assets/Scripts/PlatformController.cs
@@ -8,35 +8,93 @@ namespace MyNamespace
     {
         public LayerMask passengerMask;
         public Vector3 move;
-        
+
         public override void Start()
         {
             base.Start();
         }
-        
-        void Update()
+
+        void FixedUpdate()
         {
+            UpdateRaycastOrigins();
+
             Vector3 velocity = move * Time.deltaTime;
+            MovePassenger(velocity);
             transform.Translate(velocity);
         }
-        
+
         void MovePassenger(Vector3 velocity)
         {
+            HashSet<Transform> movedPassengers = new HashSet<Transform>();
+
             float directionX = Mathf.Sign(velocity.x);
             float directionY = Mathf.Sign(velocity.y);
-            
-            if(velocity.y != 0)
+
+            if (velocity.y != 0)
             {
                 float rayLength = Mathf.Abs(velocity.y) + skinWidth;
                 for (int i = 0; i < verticalRayCount; i++)
                 {
-                    Vector2 rayOrigin = directionY == -1 ? raycastOrigins.bottomLeft:raycastOrigins.topLeft;
+                    Vector2 rayOrigin = directionY == -1 ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
                     rayOrigin += Vector2.right * (verticalRaySpacing * i + velocity.x);
                     RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up * directionY, rayLength, passengerMask);
 
                     if (hit)
                     {
-                        float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
+                        if (!movedPassengers.Contains(hit.transform))
+                        {
+                            movedPassengers.Add(hit.transform);
+                            float pushX = (directionY == 1) ? velocity.x : 0;
+                            float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
+
+                            hit.transform.Translate(new Vector3(pushX, pushY));
+
+                        }
+                    }
+                }
+            }
+
+            if (velocity.x != 0)
+            {
+                float rayLength = Mathf.Abs(velocity.x) + skinWidth;
+                for (int i = 0; i < horizontalRayCount; i++)
+                {
+                    Vector2 rayOrigin = directionX == -1 ? raycastOrigins.bottomLeft : raycastOrigins.bottomRight;
+                    rayOrigin += Vector2.up * (horizontalRaySpacing * i);
+                    RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.right * directionX, rayLength, passengerMask);
+
+                    if (hit)
+                    {
+                        if (!movedPassengers.Contains(hit.transform))
+                        {
+                            movedPassengers.Add(hit.transform);
+                            float pushX = velocity.x - (hit.distance - skinWidth) * directionX;
+                            float pushY = -skinWidth;
+
+                            hit.transform.Translate(new Vector3(pushX, pushY));
+                        }
+                    }
+                }
+            }
+            
+            if (directionY == -1 || velocity.y == 0 && velocity.x != 0)
+            {
+                float rayLength = skinWidth * 2;
+                for (int i = 0; i < verticalRayCount; i++)
+                {
+                    Vector2 rayOrigin = raycastOrigins.topLeft + Vector2.right * (verticalRaySpacing * i);
+                    RaycastHit2D hit = Physics2D.Raycast(rayOrigin, Vector2.up, rayLength, passengerMask);
+
+                    if (hit)
+                    {
+                        if (!movedPassengers.Contains(hit.transform))
+                        {
+                            movedPassengers.Add(hit.transform);
+                            float pushX = velocity.x;
+                            float pushY = velocity.y;
+
+                            hit.transform.Translate(new Vector3(pushX, pushY));
+                        }
                     }
                 }
             }
